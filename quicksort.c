0c6f1dccd44ec694a4a5cd4697fbd717746c92d0
Bart Brock <bbrock@clarkdataservices.com>
2018-04-07 23:19:08 -0400

    added quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,8 +1,11 @@
+/* QuickSort Multithreaded */
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+
+#define ZERO 0
 
 pthread_t tid[2];
 int ret1,ret2;
@@ -29,7 +32,7 @@ void* doSomeThing(void *arg)
 
     return NULL;
 }
-
+/*
 int main(void)
 {
     int i = 0;  
@@ -55,3 +58,94 @@ int main(void)
 
     return 0;
 }
+*/
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+	array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot
+	int i = (low - 1); // Index of smaller element
+
+	for (int j = low; j <= high- 1; j++)
+	{
+		// If current element is smaller than or
+		// equal to pivot
+		if (arr[j] <= pivot)
+		{
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		int pi = partition(arr, low, high);
+
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+void extractArray(int arr[], int arr2[], int start, int size)
+{
+	for (int i = start; i < size; i++)
+		{
+			arr[i-start] = arr2[i];
+		}
+}
+// Driver program to test above functions
+int main()
+{
+	int arrLow[], arrHigh[], arr[] = {10, 7, 8, 88, 9, 1, 99, 5, 32, 29, 2, 104, 11};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	int pivot = partition(arr, 0, n-1);
+	// create sub arrays
+	int arrHighSize = n - pivot;
+	extractArray(arrHigh, arr, pivot, arrHighSize);
+	extractArray(arrLow, arr, ZERO, pivot-1);
+
+	//send upper portion to remote
+	//if ( pthread_create(&(tid[i]), NULL, &doSomeThing, NULL))
+          //          printf("\ncan't create thread :[%s]", strerror(err));
+        quickSort(arr, 0, pivot-1);
+	//wait for thread to complete
+	//pthread_join();
+	printf("Sorted array: \n");
+	printArray(arr, n);
+	return 0;
+}
+
